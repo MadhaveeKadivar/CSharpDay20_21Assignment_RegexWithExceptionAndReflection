55d3f705b86fd6641ff54359df96dca97f50a389
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-13 20:05:11 +0530

    [Madhavee] Add method for last name validation

--- UserRegistrationTesting/UserInputTesting.cs
+++ UserRegistrationTesting/UserInputTesting.cs
@@ -15,7 +15,7 @@ namespace UserRegistrationTesting
             validation = new Validation();
         }
         [TestMethod]
-        //Checking for multiple first name
+        //Checking for multiple first names
         [DataRow("Madhavee", "Input is valid")]
         [DataRow("Ab", "Input is not valid")]
         [DataRow("madhavee", "Input is not valid")]
@@ -31,6 +31,27 @@ namespace UserRegistrationTesting
                 Assert.AreEqual(expected, actual);
             }
             catch(InvalidException ex) // catch exception if input is not valid or null or empty
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+        [TestMethod]
+        //Checking for multiple last names
+        [DataRow("Kadivar", "Input is valid")]
+        [DataRow("Ab", "Input is not valid")]
+        [DataRow("kadivar", "Input is not valid")]
+        [DataRow("", "Input should not be empty")]
+        [DataRow(null, "Input should not be null")]
+        public void GivenLastNameValidation(string lastName, string expected) // Testing for Lastname Validation
+        {
+            try
+            {
+                //Act
+                string actual = validation.FirstNameValidation(lastName);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex) // catch exception if input is not valid or null or empty
             {
                 Assert.AreEqual(expected, ex.Message);
             }
--- UserRegistrationWithException/Validation.cs
+++ UserRegistrationWithException/Validation.cs
@@ -14,28 +14,58 @@ namespace UserRegistrationWithException
             try
             {
                 string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
+                if (fName == null)
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.NULL_INPUT, "Input should not be null"); //throwing exception when first name is null
+                }
                 if (fName.Equals(string.Empty))
                 {
-                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input should not be empty");
-                }                                     //If name entered by user is match with regex then it is valid otherwise not
-                if (Regex.IsMatch(fName, pattern))
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input should not be empty"); //throwing exception when first name is empty
+                }                 
+                if (Regex.IsMatch(fName, pattern)) //If name entered by user is match with regex then it is valid otherwise not
                 {
                     Console.WriteLine($"\nYour first name \"{fName}\" is valid");
                     return "Input is valid";
                 }
                 else
                 {
-                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input is not valid");
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input is not valid");//throwing exception when first name is not valid
                 }
                 
             }
-            catch (NullReferenceException ex) 
+            catch(InvalidException ex) // If any exception throws then print exception message
             {
-                throw new InvalidException(InvalidException.ExceptionType.NULL_INPUT, "Input should not be null");
+                Console.WriteLine("\n"+ex.Message);
+                return ex.Message;
             }
-            catch(InvalidException ex)
+        }
+        public string LastNameValidation(string lName) // Creating a method for First name validation
+        {
+            try
             {
-                Console.WriteLine(ex.Message);
+                string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
+                if (lName == null)
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.NULL_INPUT, "Input should not be null"); //throwing exception when first name is null
+                }
+                if (lName.Equals(string.Empty))
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input should not be empty"); //throwing exception when first name is empty
+                }
+                if (Regex.IsMatch(lName, pattern)) //If name entered by user is match with regex then it is valid otherwise not
+                {
+                    Console.WriteLine($"\nYour first name \"{lName}\" is valid");
+                    return "Input is valid";
+                }
+                else
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input is not valid");//throwing exception when first name is not valid
+                }
+
+            }
+            catch (InvalidException ex) // If any exception throws then print exception message
+            {
+                Console.WriteLine("\n"+ex.Message);
                 return ex.Message;
             }
         }
